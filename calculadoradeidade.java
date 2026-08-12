130c96a76b799989e18900ebfd8fad79143e1142
Emilly Santos <emillysantosdasilva2809@gmail.com>
2026-08-12 17:08:47 -0300

    Update calculadoradeidade.java

--- calculadoradeidade.java
+++ calculadoradeidade.java
@@ -3,6 +3,24 @@ public class CalculadoraDeIdade {
     public static void main(String[] args) {
       
        Scanner input=new Scanner(System.in);
-        System.
+        int anoatual;
+        int anoDeNascimento;
+        int idade;
+        System.out.print("Digite o ano atual em que você está:");
+        anoatual=input.nextInt();
+        System.out.print("Digite seu ano de nascimento:");
+        anoDeNascimento=input.nextInt();
+        System.out.print("Digite sua idade atual:");
+        idade=input.nextInt();
     }
 }
+
+
+//calculadoradeidade(pedir ano atual e ano em que nasceu, subtrair e imprimir: "Sua idade aproximada é [resultado da subtração] anos."//
+
+
+// média de aluno do IF (pedir duas notas, calcular a média aritmética(dividir por 2) e somar pra ter a média final//
+
+
+//Convrsos de moedas (o programa deve pedir um valor em reais e exibir em dólares. A cotação fixa pra cada 1 dólar é 5,38 reais.//
+
