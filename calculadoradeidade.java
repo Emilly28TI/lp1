42acac57d8118d4e6286aed79d608e9750803e38
Emilly Santos <emillysantosdasilva2809@gmail.com>
2026-08-12 16:44:01 -0300

    Update calculadoradeidade.java

--- calculadoradeidade.java
+++ calculadoradeidade.java
@@ -1,1 +1,15 @@
+*/calculadoraDeIdade
+  
+import java.time.LocalDate;
+import java.time.Period;
 
+public class CalculadoraIdade {
+    public static void main(String[] args) {
+        LocalDate nascimento = LocalDate.of(1995, 8, 15);
+        LocalDate hoje = LocalDate.now();
+        
+        int idade = Period.between(nascimento, hoje).getYears();
+        
+        System.out.println("A idade é: " + idade + " anos.");
+    }
+}
